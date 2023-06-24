35790f1950188734d087f0c96f6a58b94a1116f6
EduFreitas2 <137649567+EduFreitas2@users.noreply.github.com>
2023-06-24 17:10:59 -0300

    Add files via upload

---
+++ edit.js
@@ -0,0 +1,38 @@
+function is_even(first_value) {
+  if(first_value % 2 == 0)
+  {
+    return true;
+  }
+else{
+  return false;
+}
+  
+ }
+   function is_even(second_value) {
+    if(second_value % 2 == 0)
+    {
+      return true;
+    }
+  else{
+    return false;
+  }
+    
+   }
+  function add(first_value, second_value) {
+  soma = first_value + second_value
+  return soma;
+   }
+   function sub(first_value, second_value) {
+    return first_value - second_value;
+     }
+     function mul(first_value, second_value) {
+      return first_value * second_value;
+       }
+       function div(first_value, second_value) {
+        return first_value / second_value;
+         }
+        
+  
+  
+
+   
